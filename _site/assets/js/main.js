b7a6df7281e0a180f1a345b2d93b1e970adb3688
uxeskimo <uxeskimo@gmail.com>
2017-06-18 22:16:05 +0100

    New mobile nav menu

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,2 +1,7 @@
-$('html').removeClass('no-js');$(function(){$("#primary-trigger").on("click",function(e){$(".primary-nav").toggleClass("is--open"),$(this).toggleClass("is--active"),"Close main menu"==$(".show-for-sr").text()?$(".show-for-sr").text("Open main menu"):$(".show-for-sr").text("Close main menu"),e.preventDefault()})});
-
+$('html').removeClass('no-js');
+$(function() {
+    $("#primary-trigger").on("click", function(e) {
+        $(".off-canvas").toggleClass("is--open"), $(this).toggleClass("is--active"),
+        $("body").toggleClass("overflow"), $(this).toggleClass(""), e.preventDefault()
+    })
+});
