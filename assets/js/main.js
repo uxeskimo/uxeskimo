ca38a111c6d19ef78373c7ac0eb6226ba7acc84e
uxeskimo <uxeskimo@gmail.com>
2017-07-05 20:56:18 +0100

    Stremlined website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,1 @@
 $('html').removeClass('no-js');
-$(function() {
-    $("#primary-trigger").on("click", function(e) {
-        $(".off-canvas").toggleClass("is--open"), $(this).toggleClass("is--active"),
-        $("body").toggleClass("overflow"), $(this).toggleClass(""), e.preventDefault()
-    })
-});
